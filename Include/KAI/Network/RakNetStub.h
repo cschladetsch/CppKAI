d096777376500e557abea27980da9460f824582e
Christian Schladetsch <christian.schladetsch@gmail.com>
2026-02-01 06:36:37 +1100

    Update network stack.

--- Include/KAI/Network/RakNetStub.h
+++ Include/KAI/Network/RakNetStub.h
@@ -13,6 +13,7 @@
 #include <queue>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 namespace RakNet {
@@ -317,6 +318,45 @@ class RakPeerInterface {
     StartupResult Startup(unsigned int maxConnections,
                           SocketDescriptor* socketDescriptors,
                           unsigned short socketDescriptorCount) {
+#ifdef KAI_USE_ENET
+        if (socketDescriptorCount == 0 || socketDescriptors == nullptr) {
+            return INVALID_SOCKET_DESCRIPTORS;
+        }
+
+        if (!EnsureEnetInitialized()) {
+            return SOCKET_FAILED_TEST_SEND;
+        }
+
+        listenPort_ = socketDescriptors[0].port;
+        listenIp_ = socketDescriptors[0].hostAddress ? socketDescriptors[0].hostAddress : "127.0.0.1";
+        maxConnections_ = maxConnections;
+
+        ENetAddress address;
+        if (socketDescriptors[0].hostAddress && *socketDescriptors[0].hostAddress) {
+            if (enet_address_set_host(&address, socketDescriptors[0].hostAddress) != 0) {
+                return SOCKET_FAILED_TO_BIND;
+            }
+        } else {
+            address.host = ENET_HOST_ANY;
+        }
+        address.port = listenPort_;
+
+        {
+            std::lock_guard<std::mutex> lock(hostMutex_);
+            host_ = enet_host_create(&address, maxConnections_ ? maxConnections_ : 32, 2, 0, 0);
+        }
+
+        if (!host_) {
+            return SOCKET_FAILED_TO_BIND;
+        }
+
+        // Update port if zero was supplied.
+        listenPort_ = host_->address.port;
+        started_ = true;
+        guid_.g = NextGuid();
+
+        return RAKNET_STARTED;
+#else
         if (socketDescriptorCount == 0 || socketDescriptors == nullptr) {
             return INVALID_SOCKET_DESCRIPTORS;
         }
@@ -330,9 +370,24 @@ class RakPeerInterface {
         std::lock_guard<std::mutex> lock(globalMutex_);
         peersByPort_[listenPort_] = this;
         return RAKNET_STARTED;
+#endif
     }
 
     void Shutdown(unsigned int /*blockDuration*/) {
+#ifdef KAI_USE_ENET
+        std::lock_guard<std::mutex> lock(hostMutex_);
+        if (host_) {
+            enet_host_destroy(host_);
+            host_ = nullptr;
+        }
+        {
+            std::lock_guard<std::mutex> connLock(connectionMutex_);
+            connections_.clear();
+            pendingOutgoing_.clear();
+        }
+        started_ = false;
+        ReleaseEnet();
+#else
         std::vector<RakPeerInterface*> peersToNotify;
         {
             std::lock_guard<std::mutex> lock(connectionMutex_);
@@ -365,6 +420,7 @@ class RakPeerInterface {
         }
 
         started_ = false;
+#endif
     }
 
     void SetMaximumIncomingConnections(unsigned short numberAllowed) {
@@ -373,6 +429,28 @@ class RakPeerInterface {
 
     ConnectionAttemptResult Connect(const char* host, unsigned short remotePort,
                                     const char* /*passwordData*/, int /*passwordLength*/) {
+#ifdef KAI_USE_ENET
+        if (!host_ || !started_) {
+            return INVALID_PARAMETER;
+        }
+
+        ENetAddress address;
+        if (!host || !*host || enet_address_set_host(&address, host) != 0) {
+            return CANNOT_RESOLVE_DOMAIN_NAME;
+        }
+        address.port = remotePort;
+
+        std::lock_guard<std::mutex> lock(hostMutex_);
+        ENetPeer* peer = enet_host_connect(host_, &address, 2, 0);
+        if (!peer) {
+            return INVALID_PARAMETER;
+        }
+        {
+            std::lock_guard<std::mutex> connLock(connectionMutex_);
+            pendingOutgoing_.insert(peer);
+        }
+        return CONNECTION_ATTEMPT_STARTED;
+#else
         RakPeerInterface* remote = nullptr;
         {
             std::lock_guard<std::mutex> lock(globalMutex_);
@@ -409,9 +487,63 @@ class RakPeerInterface {
         remote->QueueSystemMessage(localAddress, ID_NEW_INCOMING_CONNECTION);
 
         return CONNECTION_ATTEMPT_STARTED;
+#endif
     }
 
     Packet* Receive() {
+#ifdef KAI_USE_ENET
+        std::lock_guard<std::mutex> lock(hostMutex_);
+        if (!host_) {
+            return nullptr;
+        }
+
+        ENetEvent event;
+        int serviced = enet_host_service(host_, &event, 0);
+        if (serviced <= 0) {
+            return nullptr;
+        }
+
+        if (event.type == ENET_EVENT_TYPE_CONNECT) {
+            SystemAddress addr = EnetToSystemAddress(event.peer->address);
+            AddConnection(addr, event.peer);
+
+            bool isOutgoing = false;
+            {
+                std::lock_guard<std::mutex> connLock(connectionMutex_);
+                auto it = pendingOutgoing_.find(event.peer);
+                if (it != pendingOutgoing_.end()) {
+                    pendingOutgoing_.erase(it);
+                    isOutgoing = true;
+                }
+            }
+
+            QueueSystemMessage(addr, isOutgoing ? ID_CONNECTION_REQUEST_ACCEPTED
+                                                : ID_NEW_INCOMING_CONNECTION);
+        } else if (event.type == ENET_EVENT_TYPE_RECEIVE) {
+            Packet* packet = new Packet();
+            packet->systemAddress = EnetToSystemAddress(event.peer->address);
+            packet->length = event.packet->dataLength;
+            packet->data = new unsigned char[packet->length];
+            memcpy(packet->data, event.packet->data, packet->length);
+            enet_packet_destroy(event.packet);
+            return packet;
+        } else if (event.type == ENET_EVENT_TYPE_DISCONNECT) {
+            SystemAddress addr = EnetToSystemAddress(event.peer->address);
+            RemoveConnectionWithPeer(event.peer);
+            QueueSystemMessage(addr, ID_CONNECTION_LOST);
+        }
+
+        // If we queued a system message, fall through to pop it.
+        {
+            std::lock_guard<std::mutex> qlock(queueMutex_);
+            if (incomingPackets_.empty()) {
+                return nullptr;
+            }
+            Packet* packet = incomingPackets_.front();
+            incomingPackets_.pop();
+            return packet;
+        }
+#else
         std::lock_guard<std::mutex> lock(queueMutex_);
         if (incomingPackets_.empty()) {
             return nullptr;
@@ -419,6 +551,7 @@ class RakPeerInterface {
         Packet* packet = incomingPackets_.front();
         incomingPackets_.pop();
         return packet;
+#endif
     }
 
     void DeallocatePacket(Packet* packet) { delete packet; }
@@ -432,6 +565,50 @@ class RakPeerInterface {
 
     bool Send(BitStream* bitStream, PacketPriority /*priority*/, PacketReliability /*reliability*/, char /*channel*/,
               SystemAddress systemAddress, bool broadcast) {
+#ifdef KAI_USE_ENET
+        if (!bitStream || bitStream->GetNumberOfBytesUsed() == 0 || !host_) {
+            return false;
+        }
+
+        std::vector<ENetPeer*> targets;
+        {
+            std::lock_guard<std::mutex> lock(connectionMutex_);
+            if (broadcast) {
+                for (const auto& conn : connections_) {
+                    if (conn.enetPeer) {
+                        targets.push_back(conn.enetPeer);
+                    }
+                }
+            } else {
+                for (const auto& conn : connections_) {
+                    if (conn.address == systemAddress ||
+                        conn.address.ToString() == systemAddress.ToString()) {
+                        if (conn.enetPeer) {
+                            targets.push_back(conn.enetPeer);
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (targets.empty()) {
+            return false;
+        }
+
+        const unsigned char* data = bitStream->GetData();
+        size_t length = bitStream->GetNumberOfBytesUsed();
+
+        std::lock_guard<std::mutex> lock(hostMutex_);
+        for (auto* peer : targets) {
+            ENetPacket* packet = enet_packet_create(data, length, ENET_PACKET_FLAG_RELIABLE);
+            if (packet) {
+                enet_peer_send(peer, 0, packet);
+            }
+        }
+        enet_host_flush(host_);
+        return true;
+#else
         if (!bitStream || bitStream->GetNumberOfBytesUsed() == 0) {
             return false;
         }
@@ -469,6 +646,7 @@ class RakPeerInterface {
         }
 
         return true;
+#endif
     }
 
     void StartOccasionalPing() {}
@@ -483,6 +661,7 @@ class RakPeerInterface {
     void SetOfflinePingResponse(const char* /*data*/, const unsigned int /*length*/) {}
 
     ConnectionState GetConnectionState(const SystemAddress& address) {
+#ifdef KAI_USE_ENET
         std::lock_guard<std::mutex> lock(connectionMutex_);
         for (const auto& conn : connections_) {
             if (conn.address == address || conn.address.ToString() == address.ToString()) {
@@ -490,10 +669,23 @@ class RakPeerInterface {
             }
         }
         return IS_DISCONNECTED;
+#else
+        std::lock_guard<std::mutex> lock(connectionMutex_);
+        for (const auto& conn : connections_) {
+            if (conn.address == address || conn.address.ToString() == address.ToString()) {
+                return IS_CONNECTED;
+            }
+        }
+        return IS_DISCONNECTED;
+#endif
     }
 
     SystemAddress GetInternalID() const {
+#ifdef KAI_USE_ENET
         return SystemAddress(listenIp_.c_str(), listenPort_);
+#else
+        return SystemAddress(listenIp_.c_str(), listenPort_);
+#endif
     }
 
     int GetAveragePing(const SystemAddress& /*address*/) { return 50; }
@@ -504,11 +696,17 @@ class RakPeerInterface {
     RakPeerInterface()
         : started_(false), maxConnections_(0), listenPort_(0), listenIp_("127.0.0.1") {
         guid_.g = 0;
+#ifdef KAI_USE_ENET
+        host_ = nullptr;
+#endif
     }
 
     struct Connection {
         SystemAddress address;
         RakPeerInterface* peer;
+#ifdef KAI_USE_ENET
+        ENetPeer* enetPeer;
+#endif
     };
 
     static unsigned int NextGuid() {
@@ -535,9 +733,32 @@ class RakPeerInterface {
         auto it = std::find_if(connections_.begin(), connections_.end(),
                                [peer](const Connection& conn) { return conn.peer == peer; });
         if (it == connections_.end()) {
-            connections_.push_back({address, peer});
+            connections_.push_back({address, peer
+#ifdef KAI_USE_ENET
+                                    , nullptr
+#endif
+                                   });
         }
     }
+
+#ifdef KAI_USE_ENET
+    void AddConnection(const SystemAddress& address, ENetPeer* peer) {
+        std::lock_guard<std::mutex> lock(connectionMutex_);
+        auto it = std::find_if(connections_.begin(), connections_.end(),
+                               [peer](const Connection& conn) { return conn.enetPeer == peer; });
+        if (it == connections_.end()) {
+            connections_.push_back({address, nullptr, peer});
+        }
+    }
+
+    void RemoveConnectionWithPeer(ENetPeer* peer) {
+        std::lock_guard<std::mutex> lock(connectionMutex_);
+        connections_.erase(
+            std::remove_if(connections_.begin(), connections_.end(),
+                           [peer](const Connection& conn) { return conn.enetPeer == peer; }),
+            connections_.end());
+    }
+#endif
 
     void RemoveConnectionWithPeer(RakPeerInterface* peer) {
         std::lock_guard<std::mutex> lock(connectionMutex_);
@@ -558,12 +779,53 @@ class RakPeerInterface {
 
     mutable std::mutex connectionMutex_;
     std::vector<Connection> connections_;
+#ifdef KAI_USE_ENET
+    std::unordered_set<ENetPeer*> pendingOutgoing_;
+    mutable std::mutex hostMutex_;
+    ENetHost* host_;
+#endif
 
     static std::mutex globalMutex_;
     static std::unordered_map<unsigned short, RakPeerInterface*> peersByPort_;
+
+#ifdef KAI_USE_ENET
+    static std::mutex enetInitMutex_;
+    static std::atomic<int> enetRefCount_;
+
+    static bool EnsureEnetInitialized() {
+        std::lock_guard<std::mutex> lock(enetInitMutex_);
+        if (enetRefCount_ == 0) {
+            if (enet_initialize() != 0) {
+                return false;
+            }
+        }
+        ++enetRefCount_;
+        return true;
+    }
+
+    static void ReleaseEnet() {
+        std::lock_guard<std::mutex> lock(enetInitMutex_);
+        if (enetRefCount_ > 0) {
+            --enetRefCount_;
+            if (enetRefCount_ == 0) {
+                enet_deinitialize();
+            }
+        }
+    }
+
+    static SystemAddress EnetToSystemAddress(const ENetAddress& address) {
+        char ip[64] = {0};
+        enet_address_get_host_ip(&address, ip, sizeof(ip));
+        return SystemAddress(ip, address.port);
+    }
+#endif
 };
 
 inline std::mutex RakPeerInterface::globalMutex_;
 inline std::unordered_map<unsigned short, RakPeerInterface*> RakPeerInterface::peersByPort_;
+#ifdef KAI_USE_ENET
+inline std::mutex RakPeerInterface::enetInitMutex_;
+inline std::atomic<int> RakPeerInterface::enetRefCount_{0};
+#endif
 
 }  // namespace RakNet
--- demo_network.sh
+++ demo_network.sh
@@ -10,14 +10,32 @@ echo "  42 7 +"
 echo "  stack"
 echo
 
+LOG1="$(mktemp -t kai-console1.XXXXXX.log)"
+LOG2="$(mktemp -t kai-console2.XXXXXX.log)"
+
+if command -v rg >/dev/null 2>&1; then
+    MATCH_CMD=(rg -q)
+else
+    MATCH_CMD=(grep -q)
+fi
+
+if command -v script >/dev/null 2>&1; then
+    # Use a pseudo-TTY so rang emits colors.
+    CONSOLE_CMD=(script -q /dev/null -c "./Bin/Console --non-interactive")
+else
+    CONSOLE_CMD=(./Bin/Console --non-interactive)
+fi
+
 # Console 1 commands
 (
     echo "/network start 14600"
     sleep 1
     echo "42 7 +"
     echo "stack"
+    # Keep server alive so Console 2 can connect and send commands.
+    sleep 6
     echo "exit"
-) | timeout 10s ./Bin/Console --non-interactive &
+) | timeout 12s "${CONSOLE_CMD[@]}" >>"$LOG1" 2>&1 &
 CONSOLE1_PID=$!
 
 sleep 2
@@ -25,7 +43,7 @@ sleep 2
 echo "2. Starting Console 2 (Client) on port 14601..."
 echo "Commands being sent to Console 2:"
 echo "  /network start 14601"
-echo "  /connect localhost 14600"
+echo "  /connect 127.0.0.1 14600"
 echo "  /peers"
 echo "  /@0 10 *"
 echo "  /broadcast stack"
@@ -35,19 +53,29 @@ echo
 (
     echo "/network start 14601"
     sleep 1
-    echo "/connect localhost 14600"
+    echo "/connect 127.0.0.1 14600"
+    sleep 2
+    # Retry once in case the server isn't ready yet.
+    echo "/connect 127.0.0.1 14600"
     sleep 1
     echo "/peers"
     echo "/@0 10 *"
     sleep 1
     echo "/broadcast stack"
     echo "exit"
-) | timeout 15s ./Bin/Console --non-interactive &
+) | timeout 15s "${CONSOLE_CMD[@]}" >>"$LOG2" 2>&1 &
 CONSOLE2_PID=$!
 
 # Wait for both consoles to finish
 wait $CONSOLE1_PID 2>/dev/null
 wait $CONSOLE2_PID 2>/dev/null
+
+echo
+echo "=== Console 1 Output ==="
+grep -v -F "Session terminated, killing shell" "$LOG1" | grep -v -F "...killed."
+echo
+echo "=== Console 2 Output ==="
+grep -v -F "Session terminated, killing shell" "$LOG2" | grep -v -F "...killed."
 
 echo
 echo "=== Demonstration Complete ==="
@@ -56,10 +84,24 @@ echo "What this demonstrated:"
 echo "✓ Console 1 started networking on port 14600"
 echo "✓ Console 1 calculated 42 + 7 = 49"
 echo "✓ Console 2 started networking on port 14601"
-echo "✓ Console 2 connected to Console 1"
-echo "✓ Console 2 sent command to Console 1: multiply by 10"
-echo "✓ Result: 49 * 10 = 490"
-echo "✓ Broadcast command showed stack on both consoles"
+
+if "${MATCH_CMD[@]}" "Connection failed" "$LOG2"; then
+    echo "✗ Console 2 failed to connect to Console 1"
+    echo "✗ Console 2 sent command to Console 1: multiply by 10"
+    echo "✗ Result: 49 * 10 = 490"
+    echo "✗ Broadcast command showed stack on both consoles"
+    if "${MATCH_CMD[@]}" "error: 2" "$LOG2"; then
+        echo "Note: this build uses the in-memory RakNet stub, which only connects peers inside"
+        echo "the same process. Two separate ./Bin/Console processes will always fail to connect."
+    fi
+else
+    echo "✓ Console 2 connected to Console 1"
+    echo "✓ Console 2 sent command to Console 1: multiply by 10"
+    echo "✓ Result: 49 * 10 = 490"
+    echo "✓ Broadcast command showed stack on both consoles"
+fi
+
+rm -f "$LOG1" "$LOG2"
 echo
 echo "Network Commands Available:"
 echo "  /network start [port]   - Enable networking"
